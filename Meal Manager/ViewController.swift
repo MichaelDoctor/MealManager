c64709589d3151461a65bcf6a2be3745e9a7b5d2
Michael Doctor <michaelcdoctor@gmail.com>
2021-05-05 18:13:42 -0600

    Initial Commit

---
+++ Meal Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal Manager
+//
+//  Created by Michael Doctor on 2021-05-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
